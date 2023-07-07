a863bd0b60ed4e7a41de729bc50dc3494e63421b
freeed <freeed@nudilabs.xyz>
2023-07-07 17:19:31 +0700

    ✨ (auth): add clerk
    \

--- src/components/signin-button.tsx
+++ src/components/signin-button.tsx
@@ -1,9 +1,15 @@
 import { Button } from "./ui/button";
 
+const handleClick = () => {
+  console.log("test");
+};
+
 export function Signin() {
   return (
     <div>
-      <Button size="sm">Sign In</Button>
+      <Button size='sm' onClick={handleClick}>
+        Sign In
+      </Button>
     </div>
   );
 }
