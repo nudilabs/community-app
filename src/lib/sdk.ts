d1e05da446c9f3f334719b86d9f0b291298e3657
ayvee <asianvikingofficial@gmail.com>
2023-07-15 02:54:12 +0700

    🐛 (env): update env alchemy

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -3,7 +3,7 @@ import { env } from '@/env.mjs';
 
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
-  apiKey: env.NEXT_PUBLIC_ALCHEMY_ID, // Replace with your Alchemy API Key.
+  apiKey: env.ALCHEMY_KEY, // Replace with your Alchemy API Key.
   network: Network.ETH_MAINNET, // Replace with your network.
 };
 
