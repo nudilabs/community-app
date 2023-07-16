b424fca85348185fd9c175fec6d2496a3d2aaf7c
ayvee <asianvikingofficial@gmail.com>
2023-07-17 01:01:53 +0700

    ✨ (leaderboard): disable leaderboard

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -77,7 +77,9 @@ export default function NavBar() {
               </NavigationMenuItem>
             ))}
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Leaderboard</NavigationMenuTrigger>
+              <NavigationMenuTrigger disabled>
+                Leaderboard
+              </NavigationMenuTrigger>
               <NavigationMenuContent>
                 <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                   <li className="row-span-3">
